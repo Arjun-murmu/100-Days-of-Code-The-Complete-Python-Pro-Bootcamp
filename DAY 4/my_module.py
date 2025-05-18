89a38d1d0de1a0a61d5b75b32d566fe5c402af10
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2025-05-18 11:10:47 +0000

    module

---
+++ DAY 4/my_module.py
@@ -0,0 +1,8 @@
+name = "Arjun Murmu."
+favourite_letter = 'S'
+number = 10.33
+user_name = input("Enter your name : ")
+print(f"Wow you name is {user_name}")
+
+#link : https://docs.python.org/3/library/random.html
+#Link for study : https://www.khanacademy.org/computing/computer-science/cryptography/crypt/v/random-vs-pseudorandom-number-generators
