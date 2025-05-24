0c2d5e58ffb85592acc59c03f83c1f38952ede1a
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2025-05-24 07:00:35 +0000

    DAY8

---
+++ DAY 8/love_calculation.py
@@ -0,0 +1,21 @@
+def calculate_love_score(name1 , name2):
+    combine_name = name1+name2
+    lower_name = combine_name.lower()
+    t = lower_name.count("t")
+    r = lower_name.count("r")
+    u = lower_name.count("u")
+    e = lower_name.count("e")
+    
+    first_digit = t + r + u + e
+    
+    l = lower_name.count("l")
+    o = lower_name.count("o")
+    v = lower_name.count("v")
+    e = lower_name.count("e")
+    
+    second_digit = l + o + v + e
+    
+    score = int(str(first_digit) + str(second_digit))
+    print(score)
+    
+calculate_love_score("Sonu Sankar Hansda","Jaune tudu")
