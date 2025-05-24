85e0db0e268615775b4c41a8022cb5aa3179d1bd
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2025-05-24 22:28:49 +0530

    Create function_with_input.py

---
+++ DAY 8/function_with_input.py
@@ -0,0 +1,38 @@
+#PAUSE 1
+# def greet():
+#     print("I am in function.")
+#     print("I am in function.")
+#     print("I am in function.")
+#
+# greet()
+#
+# def myFunction(input):
+#     print(f"Hey ! {input}")
+#
+# myFunction("Arjun Murmu")
+
+# def greate_input_name():
+#     name = input("Enter Your name : ")
+#     print(f"You are so beauty full {name}. ")
+# greate_input_name()
+#
+# def area_of_rectangle():
+#     width = float(input("Enter width of the rectangle :  "))
+#     length = float(input("Enter length of the rectangle : "))
+#     area = width * length
+#     perimeter = 2 * (width+length)
+#     print(f"Rectangle area is : {area.__round__(2)}")
+#     print(f"Rectangle perimeter is : {perimeter.__round__(2)}")
+#
+# area_of_rectangle()
+# area_of_rectangle()
+
+
+def life_in_weeks(age):
+    remining_year = 90 - age
+    remining_weeks = remining_year * 52
+    print(f"You have {remining_weeks} weeks left.")
+
+
+life_in_weeks(12)
+
