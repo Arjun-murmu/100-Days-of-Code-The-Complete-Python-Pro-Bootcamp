d9499f3db876da6c15b5f89584fc74d338641144
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2025-07-07 23:28:15 +0530

    Create namespace_scope.py

---
+++ DAY 12/namespace_scope.py
@@ -0,0 +1,29 @@
+from timeit import default_repeat
+
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+#--------------------Scope----------------------
+#Local Scope
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+# drink_potion()
+# # print(potion_strength)
+#
+# #Global Scope
+player_health = 10
+def game():
+    def drink_potion():
+        potion_stringth = 2
+        print(player_health)
+    drink_potion()
+print(player_health)
