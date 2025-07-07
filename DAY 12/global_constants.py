baedae6696479b739e4677f93438284f3254b090
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2025-07-07 23:26:40 +0530

    Create  global_constants.py

---
+++ DAY 12/global_constants.py
@@ -0,0 +1,15 @@
+# pi = 3.14159
+#
+# def goog():
+#     print(pi)
+#
+# goog()
+
+PI = 3.14159
+GOOGLE_URL = "https//www.google.com"
+lost = 4
+
+def access():
+    print(f"Google : {GOOGLE_URL},\n PI Value : {PI}")
+
+access()
